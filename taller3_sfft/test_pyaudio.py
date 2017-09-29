d68a3ac8a8843e90f191b1e592069d06ac3a9305
Andrés Botero <boterock@gmail.com>
2017-09-28 23:56:12 -0500

    work

--- taller3_sfft/test_pyaudio.py
+++ taller3_sfft/test_pyaudio.py
@@ -8,6 +8,7 @@ from bokeh.layouts import column
 from bokeh.client import push_session
 # from bokeh.driving import linear
 
+from scipy.io import wavfile
 
 sample_rate = 44100
 timestep = 1 / sample_rate
@@ -18,7 +19,6 @@ y = np.sin(t)
 f = np.fft.rfftfreq(y.size, timestep)
 fft = np.absolute(np.fft.rfft(y))
 
-
 sound_data = ColumnDataSource(data=dict(t=t, y=y))
 fft_data = ColumnDataSource(data=dict(f=f, fft=fft))
 
@@ -26,21 +26,22 @@ fft_data = ColumnDataSource(data=dict(f=f, fft=fft))
 def update():
     global f, fft
     f = np.fft.rfftfreq(y.size, timestep)
-    fft = np.absolute(np.fft.rfft(y))
+    # fft = np.absolute(np.fft.rfft(y))
+
+    # fft[fft < FOURIER_CUTOFF] = 0
 
     sound_data.data["t"] = t
     sound_data.data["y"] = y
     fft_data.data["f"] = f
-    fft_data.data["fft"] = fft
+    fft_data.data["fft"] = np.absolute(fft)
 
 
-figure_limit = (-0.5, 0.5)
+figure_limit = (-35000, 35000)  # i16 min to i16 max
 sound_figure = Figure(plot_width=1024, plot_height=400, y_range=figure_limit)
 sound_figure.line(x="t", y="y", source=sound_data)
 
-fft_figure = Figure(plot_width=1024, plot_height=400, y_range=(0, 200))
+fft_figure = Figure(plot_width=1024, plot_height=400, y_range=(0, 80000))
 fft_figure.line(x="f", y="fft", source=fft_data)
-
 
 document = curdoc()
 document.add_root(column(sound_figure, fft_figure))
@@ -52,26 +53,28 @@ document.add_periodic_callback(update, 1000 / 30)
 
 pa = pyaudio.PyAudio()
 
-
 use_sound_file = True
 input_file = "sax.wav"
 
-from scipy.io import wavfile
 fs, wav = wavfile.read(input_file)
 wav = wav[:, 0]
-wav = wav.astype(np.float32, order='C') / 32768.0
+# wav = wav.astype(np.float32, order='C') / 32768.0
+wav = wav.astype(np.int16, order='C')
+
 head = 0
 buff_size = 1024
 
-print(wav)
 sample_rate = fs
 timestep = 1 / sample_rate
+PA_FORMAT = pyaudio.paInt16 if wav.dtype == np.int16 else pyaudio.paFloat32
+
+FOURIER_CUTOFF = 3e4  # try 2k to 20k
 
 
 def callback(in_data, frame_count, time_info, flag):
-    data = None
 
-    global y, t, head
+    global y, t, head, fft
+    data = None
 
     if use_sound_file:
         data = wav[head:head + buff_size]
@@ -79,15 +82,33 @@ def callback(in_data, frame_count, time_info, flag):
         y = data
         t = np.arange(y.size)
 
+        fft = np.fft.rfft(y)
+
+        # encode
+        
+        fft_abs = np.absolute(fft)
+        fft[fft_abs < FOURIER_CUTOFF] = np.complex(0)
+
+        fft = (fft / 512)
+        fft = fft.view(np.float64).astype(np.uint16)
+
+        # decode
+
+        fft = fft.astype(np.float64).reshape(-1, 2).view(np.complex128)
+        fft = fft * 512
+
+        y = np.fft.irfft(fft, buff_size)
+        data = y.astype(np.int16, order='C')
+
     else:
-        y = np.fromstring(data, dtype=np.float32)
+        y = np.fromstring(data, dtype=wav.dtype)
         t = np.arange(y.size)
 
     return (data, pyaudio.paContinue)
 
 
 stream = pa.open(
-    format=pyaudio.paFloat32,
+    format=PA_FORMAT,
     channels=1,
     rate=sample_rate,
     output=use_sound_file,
